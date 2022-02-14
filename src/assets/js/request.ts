fd63f6f0d752db6990103e71755200ba0b134352
sunding <sunding@xunlei.com>
2022-02-15 00:27:41 +0800

    feat: 优化请求文件

--- src/assets/js/request.ts
+++ src/assets/js/request.ts
@@ -1,30 +1,24 @@
-import axios, { AxiosError } from 'axios'
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
 
-const createInstance = () => {
-  const instance = axios.create({
-    // baseURL, 请求前缀
-    timeout: 30000,
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-  })
+export const createInstance = (axiosConfig: AxiosRequestConfig) => {
+  const instance = axios.create(axiosConfig)
 
   /**
    * 修改请求配置
    */
   instance.interceptors.request.use(
-    (config) => config,
+    (config: AxiosRequestConfig) => config,
     (error: AxiosError) => {
       console.log(error)
       Promise.reject(error)
-    },
+    }
   )
 
   /**
    * 状态码(validateStatus) >=200 <300 执行回调1，否则走异常回调2
    */
   instance.interceptors.response.use(
-    (res) => {
+    (res: AxiosResponse) => {
       const { status, data } = res
       if (status === 200) {
         // 可以在这里统一处理200状态码下的非正常情况
@@ -41,14 +35,18 @@ const createInstance = () => {
       const { message } = error
       console.log(message)
       Promise.reject(error)
-    },
+    }
   )
 
   return instance
 }
 
-const instance = createInstance()
+const instance = createInstance({
+  // baseURL, 请求前缀
+  timeout: 30000,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+  },
+})
 
-const request = (params) => instance.request(params)
-
-export default request
+export default instance
