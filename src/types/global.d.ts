6f92c0d22e63187941c8fe8319e90cd6dc955257
sunding <sunding@xunlei.com>
2022-02-16 10:57:56 +0800

    feat: 添加d.ts模块

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,13 @@
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.bmp'
+declare module '*.css'
+declare module '*.less'
+
 declare const processEnv: {
   RUN_ENV: string
   RUN_API: string
