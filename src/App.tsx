1a1aef2bde297aba4a51b16f28316a7d12b238d7
sunding <sunding@xunlei.com>
2022-02-16 10:35:15 +0800

    feat：删除多余App.tsx多余代码

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,6 @@ import PageRoutes from './router'
 import { useRoutes } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 
-const Home = lazy(
-  () => import(/* webpackChunkName: "page-home" */ '@/pages/home')
-)
-
-const NotFound = lazy(
-  () => import(/* webpackChunkName: "page-not-found" */ '@/pages/not-found')
-)
-
-const Login = lazy(
-  () => import(/* webpackChunkName: "page-home" */ '@/pages/login')
-)
-
 const App = () => {
   return (
     <RecoilRoot>
