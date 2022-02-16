d92f57d8ddacd97dff1586f1ed71cfa4dc704d8c
sunding <sunding@xunlei.com>
2022-02-16 13:07:56 +0800

    feat: 修改.d.ts配置文件

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,13 +4,14 @@ declare module '*.jpg'
 declare module '*.jpeg'
 declare module '*.gif'
 declare module '*.bmp'
-declare module '*.bmp'
 declare module '*.css'
 declare module '*.less'
 declare module '*.scss'
 declare module '*.sass'
 declare module '*.module.css'
 declare module '*.module.less'
+declare module '*.module.scss'
+declare module '*.module.sass'
 
 declare const processEnv: {
   RUN_ENV: string
