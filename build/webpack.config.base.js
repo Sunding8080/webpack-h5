c811d9177b0394619eaf2acdaedf2a7cbca342a7
sunding <sunding@xunlei.com>
2022-02-16 11:45:54 +0800

    feat: 修改webpack配置文件

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -31,7 +31,7 @@ module.exports = {
       components: path.resolve(__dirname, '../src/components'),
       assets: path.resolve(__dirname, '../src/assets'),
     },
-    extensions: ['.js', '.json', '.ts', '.tsx', 'less', 'css', 'scss'],
+    extensions: ['.js', '.json', '.ts', '.tsx', '.less', '.css', '.scss'],
   },
 
   module: {
